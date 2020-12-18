011f8a099388d4ba31c82aae3ea7ec99cbbf19bf
Matthew J. Berger <matthewberger@nevada.unr.edu>
2020-12-17 21:04:27 -0700

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,4 @@
+pub use self::{vector::*, particle::*};
+
+mod vector;
+mod particle
---
+++ src/particle.rs
@@ -0,0 +1,7 @@
+use crate::Vector3;
+
+pub struct Particle {
+    pub position: Vector3,
+    pub velocity: Vector3,
+    pub acceleration: Vector3,
+}
